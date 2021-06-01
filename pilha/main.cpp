417fb1fb8a77f3785455e2160eb0397e92880faf
Liliane <lilianecosta767@hotmail.com>
2021-06-01 00:06:37 -0300

    finishing stack's structure

--- pilha/main.cpp
+++ pilha/main.cpp
@@ -1,20 +1,72 @@
+#include <stdlib.h>
 #include <stack>
 #include <iostream>
 
 using namespace std;
 
-int main(){
-    stack<int> pilha;
-    for(int i=0;i<10;i++){
-        pilha.push(i);
-    }
-    cout << pilha.top() << endl;
+stack<int> pilha;
+
+void push(int data) {
+    pilha.push(data);
+}
+
+void pop() {
     pilha.pop();
-    cout << pilha.top() << endl;
-    for(int i=0;i<9;i++){
-        pilha.pop();
-        if(pilha.empty()){
-            cout << "Fiquei vazia na iteração: " << i+1 << endl;
-        }
+}
+
+void top() {
+    system("clear");
+    cout << "Top: " << pilha.top();
+}
+
+void showStack() {
+    system("clear");
+    int i = 0;
+    stack<int> pilhaAux = pilha;
+    while (!pilhaAux.empty()) {
+        cout << "\n" << pilhaAux.top();
+        pilhaAux.pop();
     }
+    
+}
+
+void menu() {
+    int choice, dataPush;
+    char opctionRepeat;
+    do{
+        system("clear");
+        cout << "\n1. Push (Insert data in stack)";
+        cout << "\n2. Pop (Remove data from stack)";
+        cout << "\n3. See the top data";
+        cout << "\n4. Show stack";
+        cout << "\nPlease type your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                system("clear");
+                cout << "Please type the number to insert in the stack: ";
+                cin >> dataPush;
+                push(dataPush);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                top();
+                break;
+            case 4: 
+                showStack();
+            default: "Invalid optcion";
+        }
+        system("clear");
+        cout << "Do you wanna repeat? (s|S): ";
+        cin >> opctionRepeat;
+    } while (opctionRepeat == 's' || opctionRepeat == 'S');
+    cout << "Thanks for using my system! :)";
+}
+
+int main(){
+    menu();
+    return(0);
 }
